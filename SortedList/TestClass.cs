eef51605142af8fa677b1ba4814198bfe2a8a755
Michael Adelson <mike.adelson314@gmail.com>
2017-09-02 13:28:31 -0400

    initial solution setup

---
+++ SortedList/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedList
+{
+    public class TestClass
+    {
+        public int GetValue()
+        {
+            return 1;
+        }
+    }
+}
