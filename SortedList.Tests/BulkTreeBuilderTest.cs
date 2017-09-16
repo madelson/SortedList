f08a7fec885ea8c51ff0a0fcddfcfdfff5e6011c
Michael Adelson <mike.adelson314@gmail.com>
2017-09-15 22:43:51 -0400

    Add tests for BulkTreeBuilder

---
+++ SortedList.Tests/BulkTreeBuilderTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medallion.Collections.Tests
+{
+    public class BulkTreeBuilderTest
+    {
+        [Test]
+        public void TestBuildFromSortedList()
+        {
+            var node = BulkTreeBuilder<KeyNode<int>, int, int, KeyNode<int>.Driver>.BuildFromSortedList(new[] { 1, 2, 3, 4, 5 });
+            NodeValidator<KeyNode<int>, int>.Validate(node);
+        }
+
+        [Test]
+        public void TestBuildUnique()
+        {
+            var node = BulkTreeBuilder<KeyNode<int>, int, int, KeyNode<int>.Driver>.BuildFrom(new[] { 1, 3, 4, 2, 5 }, Comparer<int>.Default, DuplicateHandling.EnforceUnique);
+            NodeValidator<KeyNode<int>, int>.Validate(node);
+
+            Assert.Throws<ArgumentException>(
+                () => BulkTreeBuilder<KeyNode<int>, int, int, KeyNode<int>.Driver>.BuildFrom(new[] { 1, 2, 3, 4, 3 }, Comparer<int>.Default, DuplicateHandling.EnforceUnique)
+            );
+        }
+
+        [Test]
+        public void TestBuildUniqueRetailOriginal()
+        {
+            var node = BulkTreeBuilder<KeyValueNode<string, int>, string, (string, int), KeyValueNode<string, int>.Driver>.BuildFrom(
+                new[] { ("z", 1), ("b", 2), ("a", 3), ("B", 4) },
+                StringComparer.OrdinalIgnoreCase,
+                DuplicateHandling.RetainOriginal
+            );
+
+            NodeValidator<KeyValueNode<string, int>, string>.Validate(node, StringComparer.OrdinalIgnoreCase);
+            Assert.AreEqual(actual: node.Key, expected: "b");
+            Assert.AreEqual(actual: node.Value, expected: 2);
+        }
+    }
+}
---
+++ SortedList.Tests/NodeValidator.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medallion.Collections.Tests
+{
+    internal static class NodeValidator<TNode, TKey>
+        where TNode : Node<TKey, TNode>
+    {
+        public static void Validate(TNode node, IComparer<TKey> comparer = null)
+        {
+            if (node == null) { return; }
+
+            var lowerBound = Traverse.Along(node, n => n.Left).Last().Key;
+            var upperBound = Traverse.Along(node, n => n.Right).Last().Key;
+
+            InternalValidate(node, comparer ?? Comparer<TKey>.Default, lowerBound, upperBound);
+        }
+
+        private static void InternalValidate(TNode node, IComparer<TKey> comparer, TKey lowerBound, TKey upperBound)
+        {
+            var expectedCount = Node<TNode>.ComputeCount(node.Left, node.Right);
+            if (node.Count != expectedCount)
+            {
+                Assert.Fail($"Expected count {expectedCount}. Was: {node.Count}");
+            }
+            
+            if (comparer.Compare(node.Key, lowerBound) < 0)
+            {
+                Assert.Fail($"Found value '{node.Key}' below lower bound '{lowerBound}'");
+            }
+            if (comparer.Compare(node.Key, upperBound) > 0)
+            {
+                Assert.Fail($"Found value '{node.Key}' above upper bound '{lowerBound}'");
+            }
+
+            if (node.Left != null)
+            {
+                InternalValidate(node.Left, comparer, lowerBound, node.Key);
+            }
+            if (node.Right != null)
+            {
+                InternalValidate(node.Right, comparer, node.Key, upperBound);
+            }
+        }
+    }
+}
