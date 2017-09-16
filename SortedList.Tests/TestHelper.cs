04ff58ce72fc396e393a9276de730462dcb5d1d2
Michael Adelson <mike.adelson314@gmail.com>
2017-09-16 08:38:02 -0400

    Add test for indexed access

---
+++ SortedList.Tests/TestHelper.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medallion.Collections.Tests
+{
+    internal static class TestHelper
+    {
+        public static T ShouldEqual<T>(this T @this, T that, string message = null)
+        {
+            Assert.AreEqual(actual: @this, expected: that, message: message);
+            return @this;
+        }
+    }
+}
--- SortedList.Tests/WeightBalancedBinaryTreeTest.cs
+++ SortedList.Tests/WeightBalancedBinaryTreeTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Medallion.Collections.Tests
@@ -22,6 +23,23 @@ namespace Medallion.Collections.Tests
 
             tree.Add(2, allowDuplicates: true);
             Assert.AreEqual(actual: tree.Count, expected: 3);
+        }
+
+        [Test]
+        public void TestIndexedAccess()
+        {
+            var tree = new WeightBalancedBinaryTree<KeyValueNode<string, int>, string, int, (string key, int value), KeyValueNode<string, int>.Driver>(Comparer<string>.Default);
+
+            Enumerable.Range(0, 100).ToList().ForEach(i => tree.Add((i.ToString("00"), i), allowDuplicates: false));
+            
+            for (var i = 0; i < 100; ++i)
+            {
+                tree.Get(i).key.ShouldEqual(i.ToString("00"));
+                tree.Get(i).value.ShouldEqual(i);
+            }
+
+            tree.SetValue(55, -1);
+            tree["55"].ShouldEqual(-1);
         }
     }
 }
